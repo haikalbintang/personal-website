0af7150e9e15eb7b658f6939dc34de1068a18265
haikalbintang <mhaikalbintang.work@gmail.com>
2024-07-31 06:35:12 +0700

    add heading3brown and copyright.

--- mhb/src/components/Footer/index.tsx
+++ mhb/src/components/Footer/index.tsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
-    <footer className="w-full h-96 bg-[#554640] text-[#9A5D2B]">
-      <h3>ADDRESS</h3>
-    </footer>
+    <div className="px-10 bg-[#CDE6F5]">
+      <footer className="w-full bg-[#554640] text-[#9A5D2B] pt-10 px-16 pb-4 rounded-t-xl">
+        <h3 className="font-bold text-xl">ADDRESS</h3>
+        <h3 className="font-bold text-xl">CONTACT</h3>
+        <h3 className="font-bold text-xl">ACCOUNT</h3>
+        <div className="flex justify-center items-center">
+          <p className="text-[#9A5D2B]">
+            © 2024 Muhammad Haikal Bintang. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 }
