b85c4c3db5529a265f4e732273c5409b05e14638
haikalbintang <mhaikalbintang.work@gmail.com>
2024-07-31 06:18:30 +0700

    add footer and halo dunia.

--- mhb/src/app/page.tsx
+++ mhb/src/app/page.tsx
@@ -1,7 +1,17 @@
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Halo Dunia!</h1>
-    </main>
+    <>
+      <main className="bg-[#CDE6F5] flex flex-col justify-center items-center">
+        <div className="h-96 flex flex-col justify-center items-center">
+          <h1 className="bg-[#25436C] text-[#8DA7BE] text-5xl py-4 px-8 rounded-3xl">
+            Halo Dunia!
+          </h1>
+        </div>
+        <div className="h-96"></div>
+      </main>
+      <Footer />
+    </>
   );
 }
---
+++ mhb/src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full h-96 bg-[#554640] text-[#9A5D2B]">
+      <h3>ADDRESS</h3>
+    </footer>
+  );
+}
